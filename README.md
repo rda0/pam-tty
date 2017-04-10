a644f4a6a2774f2c90125b2eb20eec5807d0ee8b
Sven Mäder <maeder@phys.ethz.ch>
2017-04-10 12:43:39 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # pam_tty
 PAM module to check if the login occurs via a specific tty like a serial console.
+
+## Prerequisites
+
+Get the required PAM headers:
+
+```sh
+apt source pam
+mv pam-1.1.8 pam
+```
+
+## Clone
+
+Clone this repo:
+
+```sh
+git clone https://github.com/rda0/pam_tty.git
+```
+
+## Build
+
+```sh
+cd pam_tty
+```
+
+```sh
+gcc -fPIC -DPIC -shared -rdynamic -I../pam/libpam/include -o pam_tty.so pam_tty.c
+```
+
+Or, if you are running a multilib system, you will need to compile the PAM module for every architecture your system has a `libpam` for, for example for `Linux/x86_64` and `Linux/i386`:
+
+```sh
+gcc -m32 -fPIC -DPIC -shared -rdynamic -I../pam/libpam/include -o pam_tty_32.so pam_tty.c
+gcc -m64 -fPIC -DPIC -shared -rdynamic -I../pam/libpam/include -o pam_tty_64.so pam_tty.c
+```
+
+## Install
+
+Copy the PAM module to `/lib/security`:
+
+```sh
+mkdir /lib/security
+cp pam_tty.so /lib/security/pam_tty.so
+chmod 755 /lib/security/pam_tty.so
+chown root:root /lib/security/pam_tty.so
+```
+
+Or, on a multilib system:
+
+```sh
+cp pam_tty_32.so /lib/security/pam_tty.so
+cp pam_tty_64.so /lib64/security/pam_tty.so
+chown root:root /lib/security/pam_tty.so /lib64/security/pam_tty.so
+chmod 755 /lib/security/pam_tty.so /lib64/security/pam_tty.so
+```
+
+Enable the module in the PAM config (remove `debug` parameter for production):
+
+```sh
+# pam_tty.so will return PAM_SUCCESS if the tty is matched
+auth [success=1 default=ignore] pam_tty.so debug
+# and skip the next module (here a two factor authentication)
+auth required pam_u2f.so cue nouserok authfile=/etc/security/u2f_keys
+```
---
+++ pam_tty.c
@@ -0,0 +1,64 @@
+/* Define which PAM interfaces we provide */
+  #define PAM_SM_ACCOUNT
+  #define PAM_SM_AUTH
+  #define PAM_SM_PASSWORD
+  #define PAM_SM_SESSION
+
+  /* Include PAM headers */
+  #include <security/pam_appl.h>
+  #include <security/pam_modules.h>
+  #include <security/pam_ext.h>
+  
+  #include <unistd.h>
+  #include <syslog.h>
+  #include <stdio.h>
+  #include <string.h>
+
+  /* PAM entry point for session creation */
+  int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+          return(PAM_IGNORE);
+  }
+
+  /* PAM entry point for session cleanup */
+  int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+          return(PAM_IGNORE);
+  }
+
+  /* PAM entry point for accounting */
+  int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+          return(PAM_IGNORE);
+  }
+
+  /* PAM entry point for authentication verification */
+  int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+      int pgi_ret;
+      char *tty;
+      
+      if (PAM_SUCCESS != (pgi_ret = pam_get_item(pamh, PAM_TTY, (const void **)&tty))) {
+          pam_syslog(pamh, LOG_ERR, "Unable to obtain the tty.");
+      }
+      else {
+          pam_syslog(pamh, LOG_DEBUG, "Successfully obtained the tty.");
+          pam_syslog(pamh, LOG_DEBUG, "tty is: %s", tty);
+          if((strncmp(tty, "/dev/ttyS", 9) == 0) || (strncmp(tty, "/dev/hvc", 8) == 0)) {
+              pam_syslog(pamh, LOG_DEBUG, "tty is serial console");
+              return(PAM_SUCCESS);
+          } else {
+              pam_syslog(pamh, LOG_DEBUG, "tty is not serial console");
+          }              
+      }
+          return(PAM_IGNORE);
+  }
+
+  /*
+     PAM entry point for setting user credentials (that is, to actually
+     establish the authenticated user's credentials to the service provider)
+   */
+  int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+          return(PAM_IGNORE);
+  }
+
+  /* PAM entry point for authentication token (password) changes */
+  int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+          return(PAM_IGNORE);
+  }
