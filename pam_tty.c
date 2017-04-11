9e26f3ec66346ad8fd624eae3b387a69426f99ba
Sven Mäder <maeder@phys.ethz.ch>
2017-04-11 08:23:35 +0200

    Optimize code and add debugging

--- pam_tty.c
+++ pam_tty.c
@@ -69,54 +69,53 @@ int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
 /* PAM entry point for authentication verification */
 int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     int pgi_ret, i;
+    unsigned int pam_return = PAM_IGNORE;
     char *tty;
+    const char *arg_tty_key = "tty";
     char *arg_tty;
     char **arg_tty_values;
-//    char **res = NULL;
-//    char *p;
-//    int n_delim = 0, i;
-//
-    arg_tty = (char *)get_value("tty", argc, argv);
-//    p = strtok(arg_tty, ",");
-//    /* split string and append tokens to 'res' */
-//    while (p) {
-//        res = realloc(res, sizeof(char*) * ++n_delim);
-//        if (res == NULL) {
-//            exit(-1); /* memory allocation failed */
-//        }
-//        res[n_delim-1] = p;
-//        p = strtok (NULL, ",");
-//    }
-//
-//    /* realloc one extra element for the last NULL */
-//    res = realloc(res, sizeof(char*) * (n_delim+1));
-//    res[n_delim] = 0;
-    arg_tty_values = get_values(arg_tty, ",");
 
     if (PAM_SUCCESS != (pgi_ret = pam_get_item(pamh, PAM_TTY, (const void **)&tty))) {
-        pam_syslog(pamh, LOG_ERR, "Unable to obtain the tty.");
+        pam_syslog(pamh, LOG_ERR, "Unable to obtain tty.");
+        return(PAM_IGNORE);
     } else {
-        pam_syslog(pamh, LOG_DEBUG, "Successfully obtained the tty.");
-        pam_syslog(pamh, LOG_DEBUG, "The tty is: %s", tty);
-        if(arg_tty != NULL) {
-        for(i = 0; arg_tty_values[i] != NULL; i++) {
-        //for (i = 0; i < (n_delim+1); ++i) {
-            if(strncmp(tty, arg_tty_values[i], strlen(arg_tty_values[i])) == 0) {
-            //if(strncmp(tty, arg_tty, strlen(match_tty)) == 0) {
-            //if((strncmp(tty, "/dev/ttyS", 9) == 0) || (strncmp(tty, "/dev/hvc", 8) == 0)) {
-                pam_syslog(pamh, LOG_DEBUG, "Successfully matched tty.");
-                free(arg_tty_values); /* free the memory allocated */
-                return(PAM_SUCCESS);
-            } else {
-                pam_syslog(pamh, LOG_DEBUG, "Failed to match tty.");
-            }
-        }
-        } else {
-            pam_syslog(pamh, LOG_ERR, "Missing argment: tty");
-        }              
+        pam_syslog(pamh, LOG_DEBUG, "Successfully obtained tty: %s", tty);
     }
+
+    arg_tty = (char *)get_value(arg_tty_key, argc, argv);
+    if(arg_tty == NULL) {
+        pam_syslog(pamh, LOG_ERR, "Missing argment: %s", arg_tty_key);
+        return(PAM_IGNORE);
+    } else {
+        pam_syslog(pamh, LOG_DEBUG, "Argument: %s = %s", arg_tty_key, arg_tty);
+    }
+
+    arg_tty_values = get_values(arg_tty, ",");
+    if(arg_tty_values == NULL) {
+        pam_syslog(pamh, LOG_ERR, "Invalid argment value: %s", arg_tty_key);
+        return(PAM_IGNORE);
+    } else {
+        pam_syslog(pamh, LOG_DEBUG, "Arguments are:");
+        for(i = 0; arg_tty_values[i] != NULL; i++) {
+            pam_syslog(pamh, LOG_DEBUG, "Argument[%d] = %s", i, arg_tty_values[i]);
+        }
+    }
+
+    for(i = 0; arg_tty_values[i] != NULL; i++) {
+        if(strncmp(tty, arg_tty_values[i], strlen(arg_tty_values[i])) == 0) {
+            pam_return = PAM_SUCCESS;
+        }
+    }
+
     free(arg_tty_values); /* free the memory allocated */
-    return(PAM_IGNORE);
+
+    if(pam_return == PAM_SUCCESS) {
+        pam_syslog(pamh, LOG_DEBUG, "Successfully matched tty.");
+        return(PAM_SUCCESS);
+    } else {
+        pam_syslog(pamh, LOG_DEBUG, "Failed to match tty.");
+        return(PAM_IGNORE);
+    }
 }
 
 /*
